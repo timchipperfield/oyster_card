c77f38c0d1aec749906dc817d9c8f93ffc5718a5
Tim Chipperfield <timchipperfield@Tims-MacBook-Pro.local>
2016-05-30 15:52:52 +0100

    added attr reader for balance, starting at 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
+	it { is_expected.to respond_to(:balance) }
+
+  it "should have a balance at initialization" do
+    expect(subject.balance).to eq 0
+  end
+
 end
