dfb51091ca7905b06cc6854dd1485c2205ef3ba1
lomlo <michael.lo@live.co.uk>
2016-06-01 16:44:01 +0100

    step 12 draft, implement journey record

--- oyster/lib/oystercard.rb
+++ oyster/lib/oystercard.rb
@@ -1,14 +1,14 @@
 
 class Oystercard
 
-	attr_reader :balance, :entry_station
-  
+	attr_reader :balance, :entry_station, :journeys
+
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
 
 	def initialize
 		@balance = 0
-    @in_journey = false
+    @journeys = []
 	end
 
 	def top_up(amount)
@@ -22,18 +22,24 @@ class Oystercard
 
   def touch_in(entry_station)
     @entry_station = entry_station
+    @single_journey = []
+    @single_journey << entry_station
     fail 'Insufficient funds for journey' if check_funds
-    @in_journey = true
   end
 
-  def touch_out
+  def touch_out(exit_station)
+    @exit_station = exit_station
+    @single_journey << exit_station
     @entry_station = nil
-    deduct(1)
-    @in_journey = false
+    deduct(MINIMUM_BALANCE)
   end
 
   def in_journey?
+    @entry_station != nil
+  end
 
+  def journeys
+    @journeys << Hash[*@single_journey]
   end
 
   private
--- oyster/spec/oystercard_spec.rb
+++ oyster/spec/oystercard_spec.rb
@@ -3,6 +3,7 @@ require 'oystercard'
 describe Oystercard do
   subject(:card) { described_class.new}
   let(:entry_station) { double :entry_station }
+  let(:exit_station) { double :exit_station }
 # In order to use public transport
 # As a customer
 # I want money on my card
@@ -41,7 +42,7 @@ describe Oystercard do
     it "changes the card to be in_journey" do
       card.top_up(1)
       card.touch_in(entry_station)
-      expect(card).to be_in_journey
+      expect(card.in_journey?).to eq true
     end
     it "raises an error if the card does not have the minimum balance for a journey" do
       expect{card.touch_in(entry_station)}.to raise_error 'Insufficient funds for journey'
@@ -49,23 +50,25 @@ describe Oystercard do
   end
 
   describe "#touch_out" do
+    before(:each) do
+      card.top_up(1)
+      card.touch_in(entry_station)
+    end
     it "changes the card to not be in_journey" do
-     card.top_up(1)
-     card.touch_in(entry_station)
-     card.touch_out
+     card.touch_out(exit_station)
      expect(card).not_to be_in_journey
    end
    it "calls the #deduct method" do
      expect(card).to receive(:deduct)
-     card.touch_out
+     card.touch_out(exit_station)
    end
 
    it "should deduct 1 from the balance" do
-     expect {card.touch_out}.to change{card.balance}.by(-1)
+     expect {card.touch_out(exit_station)}.to change{card.balance}.by(-1)
    end
 
    it "should forget the journey's entry station" do
-     card.touch_out
+     card.touch_out(exit_station)
      expect(card.entry_station).to eq nil
    end
 
@@ -84,5 +87,23 @@ describe Oystercard do
     end
   end
 
+  describe "#journeys" do
+    it "shows all the trips that have been made with the card" do
+      expect(card.journeys).to be_a Array
+    end
+    it "should start with no journeys" do
+      expect(card.journeys).to eq []
+    end
+    it "contains the entry and exit station" do
+      card.top_up(10)
+      card.touch_in('station1')
+      card.touch_out('station2')
+      expect(card.journeys).to include('station1' => 'station2')
+      card.touch_in('station3')
+      card.touch_out('station4')
+      expect(card.journeys).to include('station3' => 'station4')
+      expect(card.journeys).to include('station1' => 'station2')
+    end
+  end
 
 end
